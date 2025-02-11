ac564e3fc8a7523ecb30a2edce9f2851bb0f3f64
Kshama-sh <kshama.sh0903@gmail.com>
2025-02-11 21:57:02 +0530

    First commit

---
+++ src/main/java/com/webknot/d2_assign2/D2Assign2Application.java
@@ -0,0 +1,13 @@
+package com.webknot.d2_assign2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2Assign2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D2Assign2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/webknot/d2_assign2/D2Assign2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webknot.d2_assign2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2Assign2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
